1cb2abd792c59b8e1fa176e0af7946237a220cb8
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-02-25 18:26:20 +0200

    implemented manifest

---
+++ app/manifest.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Poikainscore 2.0",
+    short_name: "Poikainscore 2.0",
+    theme_color: "#08141c",
+    background_color: "#101c24",
+    display: "standalone",
+    orientation: "portrait",
+    scope: "/",
+    start_url: "/",
+  }
+}
