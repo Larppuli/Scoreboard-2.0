cafcc13ed9f4ae95e0b2c0a05b218b25086b967f
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-03-03 20:08:11 +0200

    edited visuals

--- components/NewGamePage/SportSelection.tsx
+++ components/NewGamePage/SportSelection.tsx
@@ -10,7 +10,7 @@ export default function SportSelection({
   const icon = <IconSoccerField size={20} />;
 
   const handleChange = (value: string | null) => {
-    handleSportChange(value); // Use `null` instead of `''`
+    handleSportChange(value);
   };
 
   return (
@@ -18,7 +18,7 @@ export default function SportSelection({
       w={'100%'}
       mt={'15px'}
       leftSection={icon}
-      value={selectedSport || null} // Ensure `null` resets the selection
+      value={selectedSport || null}
       onChange={handleChange}
       placeholder="Select sport"
       data={sports}
@@ -27,11 +27,22 @@ export default function SportSelection({
           backgroundColor: '#1e1e2e',
           color: '#ffffff',
           fontSize: '16px',
-          alignContent: 'center',
           border: '1px solid #363636',
           borderRadius: '5px',
           minHeight: '50px',
           textAlign: 'center',
+        },
+        dropdown: {
+          backgroundColor: '#2e2e3e',
+          color: '#ffffff',
+          border: '1px solid #363636',
+          borderRadius: '5px',
+        },
+        option: {
+          color: '#ffffff',
+          '&:hover': {
+            backgroundColor: '#313142',
+          },
         },
       }}
     />
