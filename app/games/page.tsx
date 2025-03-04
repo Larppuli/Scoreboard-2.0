aeee7a570c783a2dcaf4d31590c3835804f48f2d
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-03-04 15:17:01 +0200

    implementation of a view displaying games

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,3 +1,12 @@
+'use client'
+
+import { Container } from "@mantine/core";
+import GamesTable from "@/components/GamesPage/GamesTable";
+
 export default function Page() {
-  return <>Games Page</>;
+  return (
+    <Container w={'100%'} bg={'#202020'} style={{ borderRadius: '10px' }}> 
+        <GamesTable />
+    </Container>
+  );
 }
