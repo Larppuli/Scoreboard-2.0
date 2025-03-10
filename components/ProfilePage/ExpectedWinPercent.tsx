dc3588faa2333fba14930f8f879dca95d2b8ba09
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-03-10 14:23:19 +0200

    fixed visuals

--- components/ProfilePage/ExpectedWinPercent.tsx
+++ components/ProfilePage/ExpectedWinPercent.tsx
@@ -5,38 +5,52 @@ export default function ExpectedWinPercent({ meanGameSize, winPercent }: Expecte
   return (
     <Group
         mt="10px"
-        px={"20px"}
-        py={'10px'}
         w={"80vw"}
         align="flex-start"
-        bg={'#141414'}
         justify='space-between'
-        style={{ borderRadius: '8px' }}
     >
-        <Stack w={'40%'} align='center'>
-            <Text fw={700} ta={'center'} >
+        <Stack
+            pb={'10px'}
+            pt={'5px'}
+            w={'47%'} 
+            align='center' 
+            bg={'#141414'}
+            style={{ borderRadius: '8px' }} >
+            <Text c={'white'} fw={700} ta={'center'} >
                 Expected win percentage
             </Text>
             <SemiCircleProgress
                 mt={-10}
+                c={'white'}
+                fw={600}
                 fillDirection="left-to-right"
                 orientation="up"
                 filledSegmentColor="#f1c40f"
+                emptySegmentColor='#36383a'
                 size={150}
                 thickness={20}
                 value={(100 / meanGameSize)}
                 label={`${(100 / meanGameSize).toFixed(1)}`}
             />
         </Stack>
-        <Stack w={'40%'} align='center' >
-            <Text fw={700} ta={'center'} >
+        <Stack
+            pb={'10px'}
+            pt={'5px'}
+            w={'47%'}
+            align='center' 
+            bg={'#141414'}
+            style={{ borderRadius: '8px' }} >
+            <Text c={'white'} fw={700} ta={'center'} >
                 Actual win percentage
             </Text>
             <SemiCircleProgress
                 mt={-10}
+                c={'white'}
+                fw={600}
                 fillDirection="left-to-right"
                 orientation="up"
                 filledSegmentColor={winPercent > 100 / meanGameSize ? 'green' : 'red'}
+                emptySegmentColor='#36383a'
                 size={150}
                 thickness={20}
                 value={winPercent}
