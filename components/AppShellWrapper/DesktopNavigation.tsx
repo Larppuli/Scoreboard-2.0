3d74b9ed4be561ab9df6cda8f3999c1d20f34497
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-04-25 14:59:49 +0300

    implemented DesktopNavigation for wider screens

---
+++ components/AppShellWrapper/DesktopNavigation.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import classes from './Demo.module.css';
+import { Group, Tabs, Text, Button } from '@mantine/core';
+import { usePathname, useRouter } from 'next/navigation';
+import {
+  IconChartBar,
+  IconSquareRoundedPlus,
+  IconList,
+  IconUserCircle,
+  IconLogout
+} from '@tabler/icons-react';
+import { useAppContext } from '@/app/lib/AppContext';
+import { audiowide } from '@/app/fonts';
+
+const navItems = [
+  { icon: IconChartBar, label: 'Stats', path: ['/'] },
+  { icon: IconSquareRoundedPlus, label: 'New game', path: ['/new-game'] },
+  { icon: IconList, label: 'Games', path: ['/games'] },
+  { icon: IconUserCircle, label: 'Profile', path: ['/profile', '/profile/achievements'] },
+];
+
+function DesktopNavigation() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const { clearContext } = useAppContext();
+
+  const handleChange = (value: string | null) => {
+    if (value && value !== pathname) {
+      router.push(value);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const res = await fetch('/api/logout', { method: 'POST' });
+      if (!res.ok) throw new Error('Logout failed');
+      router.push('/login');
+      clearContext();
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
+  return (
+    <Group 
+        bg="#08141c"
+        align='center'
+        style={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 1000,
+            borderBottom: '1px solid #0d273b',
+          }}
+    >
+        <Text c={'white'} className={audiowide.className} size='30px' mx={'4vw'}>
+            Poikainscore
+        </Text>
+        <Tabs 
+            value={pathname} 
+            onChange={handleChange} 
+            bg="#08141c"
+            color='#ff0052'
+            variant="none"
+        >
+            <Tabs.List>
+                {navItems.map((item) => {
+                const Icon = item.icon;
+                const isActive = item.path.includes(pathname);
+
+                return (
+                    <Tabs.Tab
+                        key={item.path[0]}
+                        value={item.path[0]}
+                        leftSection={<Icon size={30} color={isActive ? '#ff0052' : '#f0f0f0'} />}
+                    >
+                        <Text size="m">{item.label}</Text>
+                    </Tabs.Tab>
+                );
+                })}
+                </Tabs.List>
+            </Tabs>
+            <Button
+                onClick={handleLogout}
+                c={'#9c9c9c'}
+                w={'130px'}
+                variant="subtle"
+                size="md"
+                rightSection={<IconLogout />}
+                style={{ position: 'absolute', top: 4, right: 0 }}
+            >
+            Logout
+        </Button>
+    </Group>
+  );
+}
+
+export default DesktopNavigation;
