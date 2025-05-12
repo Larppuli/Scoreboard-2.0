6e0a82c142e4177d2efa1e35236c96e865403b41
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-05-12 22:01:26 +0300

    added checking and rendering for expired refresher token

--- app/autodarts/page.tsx
+++ app/autodarts/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Stack, Title, LoadingOverlay } from '@mantine/core';
+import { Stack, Title, LoadingOverlay, Text } from '@mantine/core';
 import AutodartsGame from '@/components/AutodartsPage/AutodartsGame';
 import { useAppContext } from '@/app/lib/AppContext';
 
@@ -17,7 +17,6 @@ export default function App() {
       .catch(err => setAutodartsGames({ error: err.message }))
       .finally(() => setLoading(false));
   }, []);
-  
 
   const playerExists = (name: string) => {
     const normalizedInput = name.toLowerCase().replace(/\s+/g, '');
@@ -28,9 +27,9 @@ export default function App() {
     });
   };
 
-  const gameAlreadyImported = (gameId: string) => 
+  const gameAlreadyImported = (gameId: string) =>
     games.some((game) => game.autodartsId === gameId);
-  
+
   const validGames = autodartsGames?.items?.filter((game: any) =>
     game.players.every((player: any) => playerExists(player.name)) &&
     !gameAlreadyImported(game.id)
@@ -38,14 +37,18 @@ export default function App() {
 
   return (
     <Stack align="center" justify="center" mt={20}>
-        <LoadingOverlay
-            visible={loading}
-            zIndex={1000}
-            overlayProps={{ bg: 'rgba(0, 0, 0, 0.5)' }}
-            loaderProps={{ color: 'red' }}
-            />
+      <LoadingOverlay
+        visible={loading}
+        zIndex={1000}
+        overlayProps={{ bg: 'rgba(0, 0, 0, 0.5)' }}
+        loaderProps={{ color: 'red' }}
+      />
 
-      {validGames?.length === 0 ? (
+      {autodartsGames?.error ? (
+        <Text c="red" size="lg" fw={500}>
+          Renew Autodarts token
+        </Text>
+      ) : validGames?.length === 0 ? (
         <Title size={16} c="gray">
           No games to export
         </Title>
