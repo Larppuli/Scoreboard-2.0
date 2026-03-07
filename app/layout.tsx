78c91baf32328d27150be68f8ab90069af97623b
Lauri Talvitie <lauri.talvitie@hotmail.com>
2026-03-07 12:58:44 +0200

    fixed font warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,24 @@ import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 
 import React from "react";
+import { Caveat } from 'next/font/google';
 import { mantineHtmlProps, MantineProvider } from "@mantine/core";
 import { AppProvider } from "@/app/lib/AppContext";
 import AppShellWrapper from "@/components/AppShellWrapper/AppShellWrapper";
 import { Metadata, Viewport } from 'next';
 
+const caveat = Caveat({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-caveat',
+});
+
 export const metadata: Metadata = {
   title: "Poikainscore 2.0",
   description: "Kovaa ajoa ja hurjaa peliä",
   icons: {
-    icon: [
-      {
-        url: "/poikainscoreFavicon.png",
-        sizes: "192x192",
-        type: "image/png",
-      }
-    ],
-    apple: [
-      {
-        url: "/poikainscoreFavicon.png",
-        sizes: "192x192", 
-        type: "image/png",
-      }
-    ]
+    icon: [{ url: "/poikainscoreFavicon.png", sizes: "192x192", type: "image/png" }],
+    apple: [{ url: "/poikainscoreFavicon.png", sizes: "192x192", type: "image/png" }]
   }
 };
 
@@ -33,15 +28,14 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
-      <head>
-      </head>
+    <html lang="en" {...mantineHtmlProps} className={caveat.variable}>
+      <head />
       <body>
-        <MantineProvider>
+        <MantineProvider defaultColorScheme="dark">
           <AppProvider>
             <AppShellWrapper>{children}</AppShellWrapper>
           </AppProvider>
